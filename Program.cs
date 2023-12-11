845ffff48cb21be31e1f7468adcd52e077c57095
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-12-10 23:04:29 -0300

    Realização da atividade sobre laços.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("--- Groot Chatbot ---\n");
+
+string pergunta;
+do
+{
+    Console.Write("Pergunta: ");
+    pergunta = Console.ReadLine()!;
+
+    if (pergunta.ToLower() == "tchau") {
+      Console.WriteLine($"                          Resposta: Eu sou Groot!");
+    } else {
+      Console.WriteLine($"                          Resposta: Eu sou Groot.");
+    }
+
+} while (pergunta.ToLower() != "tchau");
